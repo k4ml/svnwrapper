3ba4f4808767ad5115e4b45ae44d1cfcf7c4c82b
Kamal bin Mustafa <kamal@marimore.co.jp>
2013-11-29 16:36:44 +0900

    add env var to disable hooks

--- hooks/no_debug.py
+++ hooks/no_debug.py
@@ -4,6 +4,10 @@ import os
 import sys
 import commands
 
+hooks_disabled = os.environ.get('HOOKS_DISABLED', False) in (1, '1', True)
+if hooks_disabled:
+    sys.exit(0)
+
 status, output = commands.getstatusoutput("grep -r --exclude-dir='.svn' --include='*.py' 'import pdb;pdb.set_trace()' .")
 if output != '':
     print "DEBUG STATEMENT(s) found:"
